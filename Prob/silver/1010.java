5bd13241695cd4fddb777276da91cfc5623dbfef
syoooooung <110389611+syoooooung@users.noreply.github.com>
2023-10-03 22:44:06 +0900

    Update 1010.java

--- Prob/silver/1010.java
+++ Prob/silver/1010.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+            int left, right;
+            int num;
+            int [] result;
+            Scanner sc = new Scanner(System.in);
+            num=sc.nextInt();
+            result = new int[num];
+
+            for(int k=0; k<num; k++){
+                left = sc.nextInt();
+                right = sc.nextInt();
+
+                int count =1;
+                for(int j=0; j<left ; j++){
+                    count *= (right-j);
+                    count /= (j+1);
+                }
+                result[k]=count;
+                count=0;
+
+            }
+            for(int k=0; k<num ; k++){
+                System.out.println(result[k]);
+            }
+    }
+}
