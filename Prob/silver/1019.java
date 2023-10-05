702426a38a27f3c764339f78aa934ad8215933be
syoooooung <110389611+syoooooung@users.noreply.github.com>
2023-10-05 22:09:52 +0900

    Update 1019.java

--- Prob/silver/1019.java
+++ Prob/silver/1019.java
@@ -13,20 +13,21 @@ public class Main {
 
         while(true) {
             int digit = (int) (Math.log10(number));
-            if(digit<0){break;}
+            if(digit<=0){break;}
             int tmp = (int) (number / Math.pow(10, digit)); //제일 왼쪽 수가 몇인지판단
-            number -= Math.pow(10, digit) * tmp;
+            int ten = (int) (Math.pow(10, digit) * tmp); //X000.. 으로 맞추기
+            number -= ten;  //다음 수
             arr[tmp]+=(number+1);
-            if(Isfir != 0) {
-                for (int i = 0; i < 10; i++) {
-                    arr[i] += neednum(digit+1); //여기에 tmp인 경우 제외해야하지않나
+            int plusnum = neednum(digit);
+
+            for(int i=0; i<10 ; i++){
+                if(i>=tmp || i==0){
+                    arr[i]+= (plusnum* (tmp-1)) ;
                 }
-            }
-            else{
-                for (int i = 1; i < tmp; i++) {
-                    arr[i] += neednum(digit+1);
+                else{
+                    arr[i]+= Math.pow(10, digit); //그냥 pow 넣고
+                    arr[i]+= (plusnum* (tmp-1)) ;
                 }
-                Isfir=1;
             }
         }
         for(int i=1; i<number+1 ; i++){
@@ -35,14 +36,30 @@ public class Main {
 
         for(int i=0; i<10; i++)
             System.out.print(arr[i]+" ");
-
     }
 
-    static int neednum(int digit){
-        int result=0;
-        for(int i=0; i<digit ; i++)
-            result += Math.pow(10,i);
+    static int neednum(int digit){ //7XXX를 위해 함수호출했자면 digit은 3. XXX에 사용되는 각 숫자의 수
+        int result=1;
+        if(digit==1 )
+            return 1;
+        result=20;
+        for(int i=2; i<digit ; i++) {
+            result = (int) (Math.pow(10, i-1) + 9 * result);
+        }
         return result;
+    }
+
+    static void cal10(int num, int tmp, int arr[], int digit){
+        int result=0;
+        for(int i=0; i<10 ; i++){
+            if(i>=tmp || i==0){
+                arr[i]+= (neednum(digit)* (tmp-1)) ;
+            }
+            else{
+                arr[i]+= Math.pow(10, digit); //그냥 pow 넣고
+                arr[i]+= (neednum(digit)* (tmp-1)) ;
+            }
+        }
     }
 
 }
